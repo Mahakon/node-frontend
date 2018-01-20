0d1eb73433b9d02c7cbcdee1be114597fe1aa60e
Mariia Kononenko <mkon.masha@gmail.com>
2018-01-20 18:25:55 +0300

    add package.json

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(function (request, response) {
-    console.log(request.method, response.method);
-    const html = fs.readFileSync("./src/index.html", "utf8");
+    console.log(request.method, request.url);
+    const html = fs.readFileSync("./index.html", "utf8");
     response.end(html);
 });
 
 console.log("port =  " + process.env.PORT);
 
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
 
 console.log("Server is running!");
